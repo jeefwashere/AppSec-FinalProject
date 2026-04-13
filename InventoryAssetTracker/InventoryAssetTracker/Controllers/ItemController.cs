c059494a5a6e8a22f23ddb24ca099b997cff5bcd
Jeff Tieng <jtieng1377@conestogac.on.ca>
2026-04-13 07:13:26 -0400

    Refactored Item controller

--- InventoryAssetTracker/InventoryAssetTracker/Controllers/ItemController.cs
+++ InventoryAssetTracker/InventoryAssetTracker/Controllers/ItemController.cs
@@ -1,25 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InventoryAssetTracker.Controllers
 {
-    // mvc UI pages item/list/add/update
-    public class ItemController : Controller
-    {
-       
-            public IActionResult Index()
-            {
-                return View();
-            }
+	// mvc UI pages item/list/add/update
+	[Authorize]
+	public class ItemController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-            public IActionResult Add()
-            {
-                return View();
-            }
+		public IActionResult Add()
+		{
+			return View();
+		}
 
-            public IActionResult Update(int id)
-            {
-                return View();
-            }
-        }
+		public IActionResult Update(int id)
+		{
+			ViewBag.ItemId = id;
+			return View();
+		}
+	}
 }
