e2350775bbfd6bcf6ee2d1e2c28b750044e87c1e
Jeff Tieng <jtieng1377@conestogac.on.ca>
2026-04-13 07:33:48 -0400

    Implemented Add asset view model

--- InventoryAssetTracker/InventoryAssetTracker/ViewModels/AddAssetViewModel.cs
+++ InventoryAssetTracker/InventoryAssetTracker/ViewModels/AddAssetViewModel.cs
@@ -1,6 +1,13 @@
-﻿namespace InventoryAssetTracker.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryAssetTracker.ViewModels
 {
     public class AddAssetViewModel
     {
+        [Required]
+        public string AssetName { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string Quantity { get; set; }
     }
 }
